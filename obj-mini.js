3ad417873ad2b90eb95d1eff88cbbb32fc326a43
Jessica Hathaway <hathaway.jess@gmail.com>
2016-05-30 17:20:55 -0600

    Mini object project

---
+++ obj-mini.js
@@ -0,0 +1,64 @@
+//Create an object called user. Give user a name key with the value being your name, an age key with the value being your age, 
+//an email key with the value being your e-mail, and a password key with the value being a fake password.
+
+
+
+
+//Write a for in loop that will loop through the user object and console.log each key name.
+
+
+
+
+
+//Now write a for in loop that will console.log each value.
+
+
+
+
+//Change the email value to a different email.
+
+
+
+
+//Delete the password property (property refers to the key/value pair) and then console.log the user object.
+
+
+
+
+
+//Why can't you use .length on an object?
+//Answer here
+
+
+//When would you use bracket notation instead of dot notation?
+//Answer here
+
+
+
+//Below is an array of objects. Each object is representing a book you have read.
+//console.log the readingStatus of each book in this array.
+
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: false
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   },
+   {
+   	   author: 'J.R.R. Tolkein',
+   	   title: 'The Hobbit',
+   	   readingStatus: false
+   }];
+
+
+ //Remove any books not read (readingStatus:false) from the library array.
