e1bc57d622ce41e5e49d50f7398b2ca0e6487731
Jessica Hathaway <hathaway.jess@gmail.com>
2016-06-01 11:06:53 -0600

    added more practice

---
+++ more-practice.js
@@ -0,0 +1,36 @@
+//Create a car object. There should be a speed property with a numerical value and an increaseSpeed method. 
+//The increaseSpeed method will increase the speed by 5. 
+
+var car = {
+    speed: 0,
+    increaseSpeed: function(){
+        this.speed += 5;
+    }
+};
+
+//Invoke the method to increase the speed of the car.
+car.increaseSpeed();
+
+
+//Write a function that will increase the speed by any amount on any object.
+function accelerate(amount) {
+	this.speed += amount;
+}
+
+
+//Add that function as a method on the car object. Then invoke it with any number as the argument.
+
+
+car.accelerate = accelerate;
+car.accelerate(8);
+
+//Uncomment the console.log below to make sure your speed was increased.
+//console.log(car.speed)
+
+
+
+
+//Create 10 cars, where each car has a color and number. The cars should be numbered in order from 0 to 9. The cars from 0 to 4 should be colored "green" and 
+//the cars from 5 to 9 should be colored "blue". Place the cars in order in an array called carGarage.
+//HINT: Use a constructor function and the keyword this. Don't just write out 10 car objects!
+
